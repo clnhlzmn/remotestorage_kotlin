b7be3638c386867d453850e5f8d97f074f190e9d
Colin Holzman <colin.holzman@protonmail.com>
2019-05-02 16:05:50 -0400

    I runtimeClasspath adds everything to jar, including kotlin runtime, which is a problem, not including deps in jar, consumers need to add deps

--- src/main/kotlin/xyz/colinholzman/remotestorage_kotlin/Discovery.kt
+++ src/main/kotlin/xyz/colinholzman/remotestorage_kotlin/Discovery.kt
@@ -2,7 +2,10 @@ package xyz.colinholzman.remotestorage_kotlin
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import okhttp3.*
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Request
+import okhttp3.Response
 import java.io.IOException
 import java.net.URL
 import java.net.URLEncoder
