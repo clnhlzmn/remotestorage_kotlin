1e073dd9bd612b99e60130e68089d91214858443
Colin Holzman <colin.holzman@protonmail.com>
2019-05-06 12:37:06 -0400

    removed URL from Authorization

--- src/main/kotlin/xyz/colinholzman/remotestorage_kotlin/Authorization.kt
+++ src/main/kotlin/xyz/colinholzman/remotestorage_kotlin/Authorization.kt
@@ -6,17 +6,17 @@ class Authorization {
     companion object {
 
         private const val clientId = "colinholzman.xyz"
-        val redirectUrl = URL("https://rssync.colinholzman.xyz")
+        val redirectUrl = "https://rssync.colinholzman.xyz"
         private const val scope = "clipboard:rw"
 
-        fun getHref(jrd: JSONResourceDescriptor): String? {
-            return jrd.links!![0].href
+        fun getHref(jrd: JSONResourceDescriptor): String {
+            return jrd.links!![0].href!!
         }
 
-        fun getAuthQuery(jrd: JSONResourceDescriptor): URL {
+        fun getAuthQuery(jrd: JSONResourceDescriptor): String {
             val authUrlString = jrd.links?.get(0)?.properties?.get("http://tools.ietf.org/html/rfc6749#section-4.2")
             val authQuery = "$authUrlString?client_id=$clientId&response_type=token&redirect_uri=$redirectUrl&scope=$scope"
-            return URL(authQuery)
+            return authQuery
         }
 
         fun getVersion(jrd: JSONResourceDescriptor): String {
